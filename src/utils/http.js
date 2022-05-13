ec9610ae6002b94b2477c63a73d0c6d051563cdf
kenziD <1369811517@qq.com>
2022-05-13 19:03:22 +0800

    【修改】修改服务器地址

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,7 +1,7 @@
 export function get(url,data = {}){
     return new Promise((resolve,reject)=>{
         uni.request({
-            url: 'http://localhost:8080'+url, //仅为示例，并非真实接口地址。
+            url: 'http://116.205.131.72:8080'+url, //仅为示例，并非真实接口地址。
             data: data,
             success: (res) => {
                 resolve(res);
